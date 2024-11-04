58cb03af81783e610677f6358ee5a97430dbd8bc
Alejandro Garcia Anglada <aganglada@gmail.com>
2024-11-04 14:30:43 +0100

    chore: cleanup site (#51)
    Co-authored-by: github-actions <github-actions@github.com>

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -47,7 +47,7 @@ export const Header = ({
     <HeaderWrapper>
       <LogoWrapper>
         <SnapLogo color={theme.colors.icon?.default} size={36} />
-        <Title>solana-snap</Title>
+        <Title>solana-snap-wallet</Title>
       </LogoWrapper>
       <RightContainer>
         <Toggle
