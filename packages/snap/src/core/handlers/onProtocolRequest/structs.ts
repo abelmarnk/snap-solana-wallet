fc40aeb197296ccebe881beb802bdff0816db274
Alejandro Garcia Anglada <aganglada@gmail.com>
2025-03-12 10:05:42 +0100

    fix: add validation to `getGenesisHash` request (#239)

--- packages/snap/src/core/handlers/onProtocolRequest/structs.ts
+++ packages/snap/src/core/handlers/onProtocolRequest/structs.ts
@@ -1,9 +1,14 @@
-import { enums, object } from '@metamask/superstruct';
+import { array, enums, literal, object, optional } from '@metamask/superstruct';
+
+import { UuidStruct } from '../../validation/structs';
 
 export enum SolanaProtocolRequestMethod {
   GetGenesisHash = 'getGenesisHash',
 }
 
 export const SolanaGetGenesisHashRequestStruct = object({
-  method: enums([SolanaProtocolRequestMethod.GetGenesisHash]),
+  jsonrpc: literal('2.0'),
+  id: UuidStruct,
+  method: enums(Object.values(SolanaProtocolRequestMethod)),
+  params: optional(array()),
 });
